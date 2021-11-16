b6ec2ad854d2907577308b5c328e143deddbdc60
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-11-16 11:02:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace ConsoleApp3._2
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public int code { get; set; }
+        public decimal price { get; set; }
+        public Product(string name, int code, decimal price)
+        {
+            this.name = name;
+            this.code = code;
+            this.price = price;
+        }
+    }
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("Products.json", FileMode.Open))
+            {
+                Product restoredProduct = await JsonSerializer.DeserializeAsync<Product>(fs);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public int code { get; set; }
+        public decimal price { get; set; }
+        public Product(string name, int code, decimal price)
+        {
+            this.name = name;
+            this.code = code;
+            this.price = price;
+        }
+    }
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // сохранение данных
+            using (FileStream fs = new FileStream("Products.json", FileMode.OpenOrCreate))
+            {
+                Product[] products = new Product[5];
+                products[0] = new Product(Convert.ToString(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                products[1] = new Product(Convert.ToString(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                products[2] = new Product(Convert.ToString(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                products[3] = new Product(Convert.ToString(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                products[4] = new Product(Convert.ToString(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                await JsonSerializer.SerializeAsync<Product[]>(fs, products);
+                Console.WriteLine("Файл создан!");
+            }
+        }
+    }
+}
